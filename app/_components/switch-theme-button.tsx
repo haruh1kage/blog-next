dfb68312d1084f074063c82f12d6b8aea8421587
haruh1kage <1349581851@qq.com>
2024-12-11 13:31:09 +0800

    test: theme2

--- app/_components/switch-theme-button.tsx
+++ app/_components/switch-theme-button.tsx
@@ -45,7 +45,16 @@ export default function SwitchThemeButton() {
   const toggleTheme = useCallback(() => {
     console.log(theme)
     if (theme === 'system' || theme === 'light') {
-      document.documentElement.classList.remove(`dark`)
+      if(theme === 'system') {
+        if(getSystemTheme() === 'dark') {
+          document.documentElement.classList.add(`dark`)
+        } else {
+          document.documentElement.classList.remove(`dark`)
+        }
+      } else {
+        document.documentElement.classList.remove(`dark`)
+      }
+      
     } else {
       document.documentElement.classList.add(`dark`)
     }
